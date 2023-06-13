f60269ce7f8c8b34dfdfba5cd46fe636f9519946
ABarthDew <vsevolod117@gmail.com>
2023-06-13 21:46:35 +0900

    초안

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <date-picker-filter />
   </div>
 </template>
 
 <script>
+import DatePickerFilter from './store/datepicker/DatePickerFilter.vue';
 
 export default {
   name: 'App',
   components: {
+    DatePickerFilter,
   }
 }
 </script>
@@ -17,8 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 50px;
 }
 </style>
---
+++ vue2/src/store/datepicker/DatePickerFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h5>YYYY-MM-DD</h5>
+    <date-picker 
+      v-model="dateSelected"
+      :placeholder="`YYYY-MM`"
+      :format="`YYYY-MM`"
+      :range="`range`"
+    />
+  </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/index.css';
+
+export default {
+  name: 'DatePickerFilter',
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      dateSelected: new Date(),
+    }
+  }
+}
+</script>
