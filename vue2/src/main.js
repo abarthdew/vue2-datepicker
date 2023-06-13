d01d449dde52cc5f9d1ef3696fe9fec55b1d1442
ABarthDew <vsevolod117@gmail.com>
2023-06-13 21:33:28 +0900

    datepicker, vuex 설치

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+// store
+import store from './store'
+
+// vue 영역 밖에서 vue 내장 api 사용하기 위해 export 처리
+export default new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
--- vue2/src/store/index.js
+++ vue2/src/store/index.js
@@ -3,3 +3,10 @@ import Vuex from 'vuex'
 
 // modules
 import datepicker from './datepicker'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+    modules: {
+        datepicker,
+    }
+})
