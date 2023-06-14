ec646235a8a0c870cdc2fac7fdfdd2ebb8e4c6bc
ABarthDew <vsevolod117@gmail.com>
2023-06-14 19:37:16 +0900

    디렉토리 최종 정리

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import DatePickerFilter from './store/datepicker/DatePickerFilter.vue';
+import DatePickerFilter from './components/DatePickerFilter.vue';
 
 export default {
   name: 'App',
